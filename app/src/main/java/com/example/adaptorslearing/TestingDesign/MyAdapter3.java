9596568c6acda59276b034af19ad18cdc2ee07d0
Pankaj08072001 <pk9278424@gmail.com>
2026-01-12 10:37:53 +0530

    feat: Add new dependencies to `libs.versions.toml`
    Adds new version aliases and library dependencies to the version catalog (`libs.versions.toml`) to support new features.
    
    **New Versions:**
    - `circleimageview = "3.1.0"`
    - `materialVersion = "1.11.0"` (Note: a separate `material` version `1.13.0` also exists)
    - `filamentAndroid = "1.17.1"`
    
    **New Libraries:**
    - `circleimageview` for circular image views.
    - `material-v1110` for Material Components version 1.11.0.
    - `filament-android` for the Filament rendering engine.

---
+++ app/src/main/java/com/example/adaptorslearing/TestingDesign/MyAdapter3.java
@@ -0,0 +1,47 @@
+package com.example.adaptorslearing.TestingDesign;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.adaptorslearing.R;
+
+public class MyAdapter3 extends RecyclerView.Adapter<MyAdapter3.MyViewHolder2> {
+
+    String[] arr_names;
+
+    public MyAdapter3(String[] arr_names) {
+        this.arr_names = arr_names;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder2 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.single_items3, parent, false);
+        return new MyViewHolder2(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder2 holder, int position) {
+        holder.name.setText(arr_names[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return arr_names.length;
+    }
+
+    static class MyViewHolder2 extends RecyclerView.ViewHolder {
+        TextView name;
+
+        public MyViewHolder2(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.textView);
+        }
+    }
+}
